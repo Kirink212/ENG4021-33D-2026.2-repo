b8b63ab0ed08cc140c65d8b186d4896e44504f4b
Luís Fernando <lfernandotexbicalho@hotmail.com>
2026-08-28 14:36:23 -0300

    Changed "mundo" to "UNIVERSO" in print from main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("O Bicalho é o melhor professor do mundo!")
+print("O Bicalho é o melhor professor do UNIVERSO!")
 
 # git config --global user.email "lfernandotexbicalho@hotmail.com"
 # git config --global user.name "Luís Fernando"
